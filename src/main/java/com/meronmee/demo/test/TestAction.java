913235b59ab53956e91138e80a5bf821793521b2
meornmee <meronmee@163.com>
2018-01-05 16:18:18 +0800

    细化日志

--- src/main/java/com/meronmee/demo/test/TestAction.java
+++ src/main/java/com/meronmee/demo/test/TestAction.java
@@ -10,9 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.meronmee.base.controller.BaseAction;
+import com.meronmee.base.model.User;
 import com.meronmee.core.dto.JsonResult;
 import com.meronmee.core.dto.Success;
 import com.meronmee.core.utils.Assert;
+import com.meronmee.core.utils.HttpUtils;
 import com.meronmee.core.utils.RequestUtils;
 import com.meronmee.core.utils.SettingHolder;
 import com.meronmee.core.utils.SysPropHolder;
@@ -51,7 +53,16 @@ public class TestAction extends BaseAction{
         Assert.isNotBlank(msg, "msg不能为空");
         log.info("test.arg1:{}", SettingHolder.getString("test.arg1"));              
         log.info("SysPropHolder#system.arg1:{}", SysPropHolder.getString("system.arg1"));
-                
+            
+        User user = this.service.retrieveModel(User.class, 2L);
+        log.info(user.getUsername());
+        
+        try {
+			HttpUtils.post("http://yscmall.yijiahn.com/mall/demo/test.json", null);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        
     	return new Success(); 
     }
         
